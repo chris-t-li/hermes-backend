7adc34275af51c04808e6915e6e2ca870e246b3a
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-12-03 21:55:30 -0500

    user auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:username])
         if user&.authenticate(params[:password])
-            session[:username] ||= params[:username]
+            session[:user_id] = user.id
             # byebug
             # cookies[:user_id] ||="hello"
-            render json: {session: session}, status: :created
+            render json: user, status: :created
         else
-            render json: {error: {login: "Invalid username or password"}}, status: :unauthorized
+            render json: { error: "Invalid username or password" }, status: :unauthorized
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   resources :messages, except: [:index]
   # resources :users
 
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
   get '/myconversations', to: 'participants#show_my_convos'
   
 end
