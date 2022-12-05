c8d4bee58b62c0579445dfd3ac1a8b49623a4398
Chris Li <chris.lty07@gmail.com>
2022-12-05 01:33:04 -0500

    Fixed sessions error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
 
     # GET /me
     def show
-        user = User.find_by(id: session[:user_id])
+        # byebug
+        user = User.find_by(session[:user_id])
         render json: user
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # resources :participants
-  resources :conversations
-  resources :messages, except: [:index]
-  # resources :users
+  # resources :conversations
+  # resources :messages, except: [:index]
+  # # resources :users
 
-  get "/me", to: "users#show"
-  post "/signup", to: "users#create"
+  # get "/me", to: "users#show"
+  # post "/signup", to: "users#create"
 
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
