27ab5baf7d7a81ee5ef569fa2191f3b7e8f48927
Chris Li <chris.lty07@gmail.com>
2022-12-05 09:55:40 -0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,9 @@ Rails.application.routes.draw do
   # resources :messages, except: [:index]
   # # resources :users
 
-  # get "/me", to: "users#show"
-  # post "/signup", to: "users#create"
-
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  
-  # get '/myconversations', to: 'participants#show_my_convos'
   get '/myconversations', to: 'sessions#show_my_convos'
 end
