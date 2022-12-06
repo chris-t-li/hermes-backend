86345b123bc39116d9f1cac2007ff97c969997d3
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-12-06 17:08:27 -0500

    add timestamp to frontend

--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -1,5 +1,5 @@
 class ConversationSerializer < ActiveModel::Serializer
-  attributes :id, :title, :most_recent_message
+  attributes :id, :title, :most_recent_message, :most_recent_message_timestamp
   has_many :messages
   # has_many :participants
   has_many :users
@@ -8,6 +8,14 @@ class ConversationSerializer < ActiveModel::Serializer
     self.object.messages.last
   end
 
-  # if created_at == Date.today, show time as HH:MM
-  # else show time as Yesterday, or date of when message was shown
+  def most_recent_message_timestamp
+      find_message_created_at_date = self.object.messages.order(:created_at).last.created_at.to_date
+      if find_message_created_at_date == Date.today 
+          find_message_created_at.to_fs(:time)
+      elsif find_message_created_at_date == Date.yesterday
+          "Yesterday"
+      else
+          find_message_created_at_date
+      end
+  end
 end
