f3a009925351660d5833b6b536c94fd081538d51
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2023-01-31 16:30:56 -0500

    reorganized schema to work with PostgreSQL

--- db/migrate/20221208024538_add_avatar_url_to_conversation.rb
+++
@@ -1,5 +0,0 @@
-class AddAvatarUrlToConversation < ActiveRecord::Migration[7.0]
-  def change
-    add_column :conversations, :convo_url, :string
-  end
-end
--- db/migrate/20230131212101_create_conversations.rb
+++ db/migrate/20230131212101_create_conversations.rb
@@ -2,6 +2,7 @@ class CreateConversations < ActiveRecord::Migration[7.0]
   def change
     create_table :conversations do |t|
       t.string :title
+      t.string :convo_url
 
       t.timestamps
     end
